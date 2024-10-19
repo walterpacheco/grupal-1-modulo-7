5feaa80fcfdf16876e118dbc6c256d235e0b8d25
walterpacheco <walter.e.pacheco.chandia@gmail.com>
2024-10-18 22:03:16 -0300

    se midica vistas

--- app.js
+++ app.js
@@ -1,117 +1,39 @@
 const express = require('express');
-const { Client } = require('pg');
+const path = require('path');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+
+const estudianteController = require('./controllers/estudianteController');
 
 dotenv.config();
 
 const app = express()
 
-const client = new Client({
-    user: process.env.DB_USUARIO,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NOMBRE,
-    password: process.env.DB_CONTRASENA,
-    port: process.env.DB_PUERTO,
-    ssl: false
-
-});
-
-// Conectar a la base de datos
-client.connect();
-
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//ruta principal
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
-async function agregarEstudiante(nombre, rut, curso, nivel) {
-    const query = 'INSERT INTO estudiantes (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *';
-    const values = [nombre, rut, curso, nivel];
-    try {
-        const res = await client.query(query, values);
-        console.log('Estudiante agregado:', res.rows[0]);
-    } catch (err) {
-        console.error('Error al agregar el estudiante:', err);
-    }
-}
+//rutas
+app.get('/agregar', estudianteController.formularioAgregar);
+app.get('/agregar', estudianteController.agregarEstudiante);
 
-async function consultarEstudiantes() {
-    try {
-        const res = await client.query('SELECT * FROM estudiantes');
-        console.log('Estudiantes registrados:', res.rows);
-    } catch (err) {
-        console.error('Error al consultar los estudiantes:', err);
-    }
-}
+app.get('/consultar', estudianteController.consultarEstudiantes);
+app.get('/consultar-rut', estudianteController.formularioConsultarRut);
+app.post('/consultar-rut', estudianteController.consultarEstudiantePorRut);
 
-async function consultarEstudiantePorRut(rut) {
-    const query = 'SELECT * FROM estudiantes WHERE rut = $1';
-    const values = [rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rows.length > 0) {
-            console.log('Estudiante encontrado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT.');
-        }
-    } catch (err) {
-        console.error('Error al consultar por RUT:', err);
-    }
-}
+app.get('/actualizar/:rut', estudianteController.formularioActualizar);
+app.post('/actualizar', estudianteController.actualizarEstudiante);
 
-async function actualizarEstudiante(nombre, rut, curso, nivel) {
-    const query = 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4 RETURNING *';
-    const values = [nombre, curso, nivel, rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rowCount > 0) {
-            console.log('Estudiante actualizado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT para actualizar.');
-        }
-    } catch (err) {
-        console.error('Error al actualizar el estudiante:', err);
-    }
-}
-async function eliminarEstudiante(rut) {
-    const query = 'DELETE FROM estudiantes WHERE rut = $1 RETURNING *';
-    const values = [rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rowCount > 0) {
-            console.log('Estudiante eliminado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT para eliminar.');
-        }
-    } catch (err) {
-        console.error('Error al eliminar el estudiante:', err);
-    }
-}
-
-const action = process.argv[2];
-const args = process.argv.slice(3);
-
-switch (action) {
-    case 'agregar':
-        agregarEstudiante(args[0], args[1], args[2], args[3]);
-        break;
-    case 'consultar':
-        consultarEstudiantes();
-        break;
-    case 'consultar-rut':
-        consultarEstudiantePorRut(args[0]);
-        break;
-    case 'actualizar':
-        actualizarEstudiante(args[0], args[1], args[2], args[3]);
-        break;
-    case 'eliminar':
-        eliminarEstudiante(args[0]);
-        break;
-    default:
-        console.log('Acción no reconocida. Usa "agregar", "consultar", "consultar-rut", "actualizar" o "eliminar".');
-        break;
-}
+app.get('/eliminar', estudianteController.formularioEliminar);
+app.post('eliminar', estudianteController.eliminarEstudiante);
 
 
 const PUERTO = process.env.PUERTO || 3000;
--- services/estudianteServicio.js
+++ services/estudianteServicio.js
@@ -18,65 +18,34 @@ client.connect();
 async function agregarEstudiante(nombre, rut, curso, nivel) {
     const query = 'INSERT INTO estudiantes (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *';
     const values = [nombre, rut, curso, nivel];
-    try {
-        const res = await client.query(query, values);
-        console.log('Estudiante agregado:', res.rows[0]);
-    } catch (err) {
-        console.error('Error al agregar el estudiante:', err);
-    }
+    const res = await client.query(query, values);
+    return res.rows[0];
 }
 
 async function consultarEstudiantes() {
-    try {
-        const res = await client.query('SELECT * FROM estudiantes');
-        console.log('Estudiantes registrados:', res.rows);
-    } catch (err) {
-        console.error('Error al consultar los estudiantes:', err);
-    }
+    const res = await client.query('SELECT * FROM estudiantes');
+    return res.rows;
 }
 
 async function consultarEstudiantePorRut(rut) {
     const query = 'SELECT * FROM estudiantes WHERE rut = $1';
     const values = [rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rows.length > 0) {
-            console.log('Estudiante encontrado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT.');
-        }
-    } catch (err) {
-        console.error('Error al consultar por RUT:', err);
-    }
+    const res = await client.query(query, values);
+    return res.rows.length > 0 ? res.rows[0] :null;
 }
 
 async function actualizarEstudiante(nombre, rut, curso, nivel) {
     const query = 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4 RETURNING *';
     const values = [nombre, curso, nivel, rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rowCount > 0) {
-            console.log('Estudiante actualizado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT para actualizar.');
-        }
-    } catch (err) {
-        console.error('Error al actualizar el estudiante:', err);
-    }
+    const res = await client.query(query, values);
+    return res.rowsCount > 0 ? res.rows[0] :null;
 }
+
 async function eliminarEstudiante(rut) {
     const query = 'DELETE FROM estudiantes WHERE rut = $1 RETURNING *';
     const values = [rut];
-    try {
-        const res = await client.query(query, values);
-        if (res.rowCount > 0) {
-            console.log('Estudiante eliminado:', res.rows[0]);
-        } else {
-            console.log('No se encontró ningún estudiante con ese RUT para eliminar.');
-        }
-    } catch (err) {
-        console.error('Error al eliminar el estudiante:', err);
-    }
+    const res = await client.query(query, values);
+    return res.rowsCount > 0 ? res.rows[0] :null;
 }
 
 module.exports = {
